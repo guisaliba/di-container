803dc00c1332a205a65aaac6b1bc14f255012ccf
Guilherme Saliba <salibagui19@gmail.com>
2025-01-22 00:17:05 -0300

    feat: add `C#` container implementation

---
+++ src/Program.cs
@@ -0,0 +1,8 @@
+﻿using src.interfaces;
+using src.models;
+
+IA aImplementation = new AImpl();
+IB bImplementation = new BImpl();
+
+aImplementation.SetB(bImplementation);
+Console.WriteLine(aImplementation.AJob()); // I am A and I depend on B: 'I am B'
