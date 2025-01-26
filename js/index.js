9c114068eb14a99a3ad4f0bb9b8bdf918589e388
Guilherme Saliba <salibagui19@gmail.com>
2025-01-26 13:46:28 -0300

    chore: remove js implementation

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import { Container } from "./container.js";
-import { UserService, ConfigurableService } from "./service.js";
-
-const container = new Container();
-
-container.register("UserService",UserService);
-container.register("ConfigurableService",ConfigurableService);
-
-const myService = container.resolve("ConfigurableService");
