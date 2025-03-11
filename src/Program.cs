94b5c2df1b532ed83ce650d43ec4b7cd14561c7f
Guilherme Saliba <salibagui19@gmail.com>
2025-03-11 13:07:59 -0300

    *

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,8 @@
 ﻿using src.interfaces;
 using src.models;
 
-IA aImplementation = new AImpl();
-IB bImplementation = new BImpl();
+IAInterface aImplementation = new AImplementation();
+IBInterface bImplementation = new BImplementation();
 
 aImplementation.SetB(bImplementation);
 Console.WriteLine(aImplementation.AJob()); // I am A and I depend on B: 'I am B'
